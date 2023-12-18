846c9ee5df1e86f6341d7b0cb48dcedc1877cf30
Henri <104494267+H1nr1@users.noreply.github.com>
2023-12-17 23:45:15 -0500

    Update reaction_handler.go

--- giveaway/reaction_handler.go
+++ giveaway/reaction_handler.go
@@ -1,20 +1,40 @@
 {{/* Reaction: Added + Removed */}}
 
-{{$Emoji:="🎉"}} {{/* Emoji to use as reaction */}}
+{{$emoji:="🎉"}} {{/* emoji to use as reaction */}}
 {{$errCID:=.Channel.ID}} {{/* ID of channel to send errors */}}
 
-{{if and (eq .Reaction.Emoji.Name $Emoji) .Message.Embeds}}
-	{{if ($G:=(dbGet ($GID:=toInt (reFind `\d+` (index .Message.Embeds 0).Footer.Text)) "Giveaways").Value)}}
+{{if and (eq .Reaction.Emoji.Name $emoji) .Message.Embeds}}
+	{{$e:=sdict 
+		"title" (printf "Giveaway Hosted in %s" .Guild.Name) 
+		"color" 16711680
+	}}
+	{{if $g:=(dbGet ($GID:=(index .Message.Embeds 0).Footer.Text|reFind `\d+`|toInt) "giveaways").Value}}
 		{{if .ReactionAdded}}
-			{{$G.Set "Users" ($G.Users.Append .User.ID)}}{{dbSet $GID "Giveaways" $G}}
-			{{sendDM (cembed "title" (print "Giveaway Hosted in " .Guild.Name) "description" (printf "Your entry into the giveaway for **%s** has been confirmed!" $G.Prize) "color" 65280)}}
+			{{$g.Set "users" ($g.users.Append .User.ID)}}
+			{{$e.Set "description" (printf "Your entry into the giveaway for **%s** has been confirmed!" 
+				$g.prize
+			)}}
+			{{$e.Set "color" 65280}}
 		{{else}}
-			{{$Users:=cslice}}{{range $G.Users}}{{- if ne . $.User.ID}}{{- $Users =$Users.Append .}}{{- end -}}{{end}}
-			{{$G.Set "Users" $Users}}{{dbSet $GID "Giveaways" $G}}
-			{{sendDM (cembed "title" (print "Giveaway Hosted in " .Guild.Name) "description" (printf "Your entry into the giveaway for **%s** has been removed" $G.Prize) "color" 16711680)}}
+			{{$users:=cslice}}
+			{{range $g.users}}
+				{{- if ne . $.User.ID}}
+					{{- $users =$users.Append .}}
+				{{- end -}}
+			{{end}}
+			{{$g.Set "users" $users}}
+			{{$e.Set "description" (printf "Your entry into the giveaway for **%s** has been removed" 
+				$g.prize
+			)}}
 		{{end}}
+		{{dbSet $GID "giveaways" $g}}
 	{{else}}
-		{{sendDM (cembed "title" (print "Giveaway Hosted in " .Guild.Name) "description" (printf "Your entry into the giveaway for **%s** could not be confirmed\nPlease re-react to [the giveaway](<%s>)\n*Staff have been notified of the failed entry*" $G.Prize .Message.Link) "color" 16711680)}}
-		{{sendMessage $errCID (printf "%s (%d)'s attempt to join giveaway `%d` failed" .User.Username .User.ID $GID)}}
+		{{$e.Set "description" (printf "Your entry into the giveaway for **%s** could not be confirmed\nPlease re-react to [the giveaway](<%s>)\n*Staff have been notified of the failed entry*" 
+			$g.prize .Message.Link
+		)}}
+		{{sendMessage $errCID (printf "%s (%d)'s attempt to join giveaway `%d` failed" 
+			.User.Username .User.ID $GID
+		)}}
 	{{end}}
+	{{sendDM (cembed $e)}}
 {{end}}
