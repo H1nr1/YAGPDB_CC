5343121ddee39967836820ede24768d7f16c6e59
Henri <104494267+H1nr1@users.noreply.github.com>
2024-06-24 16:55:48 -0400

    Update main.go

--- giveaway/main.go
+++ giveaway/main.go
@@ -1,6 +1,13 @@
-{{/* Regex: `\A\-g(ive)?a(wa)?y?` */}}
+{{/* Regex \A\-g(ive)?a(wa)?y? */}}
+
+{{$IDs:=(dbGet 0 "IDs").Value}}
+{{$RIDs:=$IDs.role}}{{$CIDs:=$IDs.channel}}{{$MIDs:=$IDs.msg}}{{$emoji:=$IDs.emoji}}
 
 {{$emoji:="🎉"}} {{/* emoji to use as reaction */}}
+{{/* if "word" is in prize, give role of ID; "word" should be lowercase */}}
+{{$keyWords:=sdict 
+	"word" 0
+}}
 
 {{if .ExecData}}
 	{{$g:=(dbGet .ExecData "giveaways").Value}}
@@ -9,24 +16,33 @@
 	  	"title" "🌟 Giveaway Ended! 🌟" 
 		"thumbnail" (sdict "url" "https://media.discordapp.net/attachments/1036894710343671918/1070947166241173514/GL_Giveaway.png?width=419&height=419") 
 		"footer" (sdict 
-			"text" (printf "ID: %#v | Ended " .ExecData)
+			"text" (printf "ID: %d | Ended " .ExecData)
 		) 
 	  	"timestamp" currentTime 
 		"color" 13938487
 	}}
-	{{if $g.Users}}
-		{{$winCount:=len $g.users|min $g.winCount|toInt}}
+	{{if $g.users}}
+		{{$winCount:=$g.winCount}}
+		{{if gt $winCount (len $g.users)}}{{$winners =$g.users}}{{$winCount =0}}{{end}}
 		{{while gt $winCount 0}}
-			{{- $winner:=len $g.users|add 1|randInt|index $g.users}}
-			{{- if not (in $winners $winner)}}
-				{{- $winners =$winners.Append $winner}}
-				{{- $winCount =sub $winCount 1}}
+			{{- $winner:=len $g.users|add -1|randInt|index $g.users}}
+			{{- if in $winners $winner}}{{- continue}}{{- end}}
+			{{- $winners =$winners.Append $winner}}
+			{{- $winCount =sub $winCount 1}}
+		{{end}}
+		{{$roleToGive:=0}}
+		{{range $k,$v:=$keyWords}}
+			{{- if in (lower $g.prize) $k}}
+				{{- $roleToGive =$v}}
+				{{- break}}
 			{{- end -}}
 		{{end}}
-		{{$mentWinners:=""}}
-		{{range $winners}}
-			{{- $mentWinners =printf "%s<@%d>" $mentWinners . -}}
+		{{if $roleToGive}}
+			{{range $winners}}
+				{{- giveRoleID . $roleToGive}}
+			{{end}}
 		{{end}}
+		{{$mentWinners:=""}}{{range $winners}}{{- $mentWinners =printf "%s<@%d>" $mentWinners . -}}{{end}}
 		{{$e.Set "description" (printf "**__Prize:__ %s\n__Entries:__ %d\n__Winners:__** %s" 
 			$g.prize (len $g.users) $mentWinners
 		)}}
@@ -37,11 +53,7 @@
 		{{editMessage nil $g.MID (cembed $e)}}
 	{{end}}
 	{{deleteAllMessageReactions nil $g.MID}}
-	{{dbSetExpire .ExecData "giveaways" (sdict 
-		"prize" $g.prize 
-		"users" $g.users 
-		"winners" $winners
-	) 86400}}
+	{{dbSetExpire .ExecData "giveaways" (sdict "prize" $g.prize "users" $g.users "winners" $winners) 86400}}
 	{{return}}
 {{end}}
 
@@ -69,40 +81,41 @@
 	{{$dur:=$args.Get 1}}
 	{{$winCount:=$args.Get 2}}
 	{{$prize:=$args.Get 3}}
+	{{$gID:=dbIncr 0 "giveaways" 1|toInt}}
 	{{$id:=sendMessageRetID nil (cembed 
 		"title" "🌟 Giveaway 🌟" 
-		"description" (printf "**__Prize:__ %s\n__Winners:__ %d\n__Duration:__ %s\n\nReact with %s to enter the Giveaway**" 
-			$prize $winCount (humanizeDurationMinutes $dur) $emoji
+		"description" (printf "**__Prize:__ %s\n__Winners:__ %d\n\nReact with %s to enter the Giveaway**" 
+			$prize $winCount $emoji
 		) 
 		"thumbnail" (sdict "url" "https://media.discordapp.net/attachments/1036894710343671918/1070947166241173514/GL_Giveaway.png?width=419&height=419") 
 		"footer" (sdict 
-			"text" (printf "ID: %d | Ends " ($GID:=dbIncr 0 "giveawayID" 1|toInt))
+			"text" (printf "ID: %d | Ends " $gID)
 		) 
 		"timestamp" (currentTime.Add $dur) 
 		"color" 13938487
 	)}}
 	{{addMessageReactions nil $id $emoji}}
-	{{dbSet $GID "giveaways" (sdict 
+	{{dbSet $gID "giveaways" (sdict 
 		"MID" $id 
 		"prize" $prize 
 		"winCount" $winCount 
 		"users" cslice
 	)}}
-	{{scheduleUniqueCC .CCID nil $dur.Seconds $GID $GID}}
+	{{scheduleUniqueCC .CCID nil $dur.Seconds $gID $gID}}
 
 {{else if eq $subCmd "e" "end"}}
 	{{$args =parseArgs 2 "" 
 		(carg "string" "") 
 		(carg "int" "")
 	}}
-	{{$GID:=$args.Get 1}}
-	{{if not (dbGet $GID "giveaways")}}
+	{{$gID:=$args.Get 1}}
+	{{if not (dbGet $gID "giveaways")}}
 		Invalid Giveaway ID
 		{{deleteResponse}}
 		{{return}}
 	{{end}}
-	{{cancelScheduledUniqueCC .CCID $GID}}
-	{{execCC .CCID nil 1 $GID}}
+	{{cancelScheduledUniqueCC .CCID $gID}}
+	{{execCC .CCID nil 0 $gID}}
 
 {{else if eq $subCmd "rr" "reroll" "rroll"}}
 	{{$args =parseArgs 3 "" 
@@ -110,9 +123,9 @@
 		(carg "int" "") 
 		(carg "int" "")
 	}}
-	{{$GID:=$args.Get 1}}
+	{{$gID:=$args.Get 1}}
 	{{$amt:=$args.Get 2}}
-	{{$g:=(dbGet $GID "giveaways").Value}}
+	{{$g:=(dbGet $gID "giveaways").Value}}
 	{{if not $g}}
 		Invalid Giveaway ID
 		{{deleteResponse}}
@@ -134,13 +147,15 @@
 			{{- $users =$users.Append .}}
 		{{- end -}}
 	{{end}}
+	{{$newWinners:=cslice}}
 	{{while gt $amt 0}}
-		{{$winner:=len $users|add 1|randInt 0|index $users}}
+		{{$winner:=len $users|add 1|randInt|index $users}}
+		{{$newWinners =$newWinners.Append $winner}}
 		{{$g.Set "winners" ($g.winners.Append $winner)}}
 		{{$amt =sub $amt 1}}
 	{{end}}
 	{{$mentWinners:=""}}
-	{{range $g.winners}}
+	{{range $newWinners}}
 		{{- $mentWinners =printf "%s <@%d>" $mentWinners . -}}
 	{{end}}
 	{{printf "*Giveaway was rerolled %d times*\n**__Prize:__ %s\n__New Winners:__** %s" 
